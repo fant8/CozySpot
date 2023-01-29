274db79200e6fb10f9234e692a2fa71a67ecb84d
Kevin Cheung <cheunk20@mcmaster.ca>
2023-01-28 20:18:03 -0500

    fix cringe error

---
+++ client/src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Blog = () => {
+
+    return(
+        <>
+        <h1>Hello!</h1>
+
+        <h2>Your recently listened:</h2>
+        </>
+    )
+}
+
+export default Blog 
