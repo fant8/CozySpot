3b54530cfe86922e91b7a618d3294026297db2b5
Amy Fan <amyfan@Amys-MacBook-Air.local>
2023-01-31 14:33:19 -0500

    pushing some schema stuff

--- server/server.js
+++ server/server.js
@@ -29,23 +29,6 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-<<<<<<< HEAD
-
-/* For spotify API */
-
-/* Load the HTTP library */
-var http = require('http');
-
-/* Create an HTTP server to handle responses */
-
-http
-  .createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World');
-    response.end();
-  })
-  .listen(8888);
-=======
 function generateRandomString(length){
   const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numChars = chars.length;
@@ -73,4 +56,3 @@ app.get('/login_successful', function(req, res) {
   })
 
 
->>>>>>> fd70291da010f3774758bc88a4d21e3fcb39423f
