2bfbfe6a42439db3236cc53414b2468154a24d4e
Lonnie Kim <kims171@mcmaster.ca>
2023-01-31 14:34:25 -0500

    made top songs pretty

--- client/src/components/profileSong.js
+++ client/src/components/profileSong.js
@@ -1,13 +1,15 @@
 import React from "react";
 import '../profile_src/profile.css'
 
-const ProfileArtist = (props) => {
+const ProfileSong = (props) => {
     return (
-        <div>
-            {/* TODO Need to add artist and album of the song */} 
-            <p>{props.name}</p>
-        </div>
+    <div class="grid-container">
+        <div class="grid-item"><img style={{width: "70%", height: "100%"}} src={props.image}></img></div>
+        <div class="grid-item">{props.name}</div>
+        <div class="grid-item">{props.artist}</div>
+        <div class="grid-item">{props.album}</div>
+    </div>
     )
 }
 
-export default ProfileArtist;
+export default ProfileSong;
--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -17,14 +17,14 @@ const Profile = () => {
     return(
         <div classname="UserProfile">
             {/* User Profile */} 
-            <img class="center" style={{width: "20%", height: "10%"}} src={pro_pic}></img>
+            <img class="center" style={{marginTop: "3%", width: "20%", height: "10%"}} src={pro_pic}></img>
             <center><h1>Lon</h1></center>
-            <center><h5>"Changmo is absoulutely my fav artist fr fr my bffl no cap"</h5></center>
+            <center><h5>"Music is a moral law. It gives soul to the universe."</h5></center>
             <img class="center" style={{width: "2%", height: "2%"}} src={add_friend}></img>
 
             {/* Playlists */} 
             <h2>Featured Playlists:</h2>
-            <img class = "right" style={{width: "7%", height: "4%"}} src={view_more}></img>
+            <img class = "right" style={{width: "9%", height: "6%"}} src={view_more}></img>
             <div class="row">
                 <div class="column">
                     <img src={playlist1} style={{width: "100%", height: "70%"}}></img>
@@ -46,14 +46,35 @@ const Profile = () => {
 
             {/* Tracks */}
             <h2>Top Tracks this month:</h2>
-            <p>My Ninjas - Fleeky Bang</p>
-            <p>New thing (Prod. ZICO) (Feat.Homies)</p>
-            <p>POWER - Kanye West</p>
-            <p>We Are - Woo</p>
-            <p>Jet Lag - Blase, Chillin Homie</p>
+            <img class = "right" style={{width: "9%", height: "6%"}} src={view_more}></img>
+            <div class="grid-container">
+                <div class="grid-item"><img style={{width: "70%", height: "100%"}} src={playlist1}></img></div>
+                <div class="grid-item">Power</div>
+                <div class="grid-item">Kanye West</div>
+                <div class="grid-item">My Beautiful Dark Twisted Fantasy</div>
+            </div>
+            <div class="grid-container">
+                <div class="grid-item"><img style={{width: "70%", height: "100%"}} src={playlist2}></img></div>
+                <div class="grid-item">Maestro</div>
+                <div class="grid-item">Changmo</div>
+                <div class="grid-item">DBSG 2</div>
+            </div>
+            <div class="grid-container">
+                <div class="grid-item"><img style={{width: "70%", height: "100%"}} src={playlist3}></img></div>
+                <div class="grid-item">New Thing (Prod. Zico) (Feat. Homies)</div>
+                <div class="grid-item">Zico</div>
+                <div class="grid-item">Street Man Fighter EP 2</div>
+            </div>
+            <div class="grid-container">
+                <div class="grid-item"><img style={{width: "70%", height: "100%"}} src={playlist4}></img></div>
+                <div class="grid-item">Mama Lisa</div>
+                <div class="grid-item">Swervy</div>
+                <div class="grid-item">Undercover Angel</div>
+            </div>
 
             {/* Artists */}
-            <h2>Top Artists this month:</h2>
+            <h2 style={{marginTop: "5%"}}>Top Artists this month:</h2>
+            <img class = "right" style={{width: "9%", height: "6%"}} src={view_more}></img>
             <div class="row">
                 <div class="column">
                     <img src={playlist1} style={{width: "100%", height: "70%"}}></img>
@@ -75,6 +96,7 @@ const Profile = () => {
 
             {/* Albums */}
             <h2>Top Albums this month:</h2>
+            <img class = "right" style={{width: "9%", height: "6%"}} src={view_more}></img>
             <div class="row">
                 <div class="column">
                     <img src={playlist1} style={{width: "100%", height: "70%"}}></img>
